ea690e6c91f7409edea463273f185e6074b38429
stepan <stepangg@yandex.ru>
2026-07-05 13:03:26 +0300

    поле может быть nll

--- alembic/versions/sql/46fd2c011852/up.sql
+++ alembic/versions/sql/46fd2c011852/up.sql
@@ -1,10 +1,5 @@
 ALTER TABLE sales.orders ADD COLUMN created_by INTEGER;
 
-UPDATE sales.orders SET created_by = 1 WHERE created_by IS NULL;
-
-
-ALTER TABLE sales.orders ALTER COLUMN created_by SET NOT NULL;
-
 ALTER TABLE sales.orders
     ADD CONSTRAINT fk_order_created_by
     FOREIGN KEY (created_by)
